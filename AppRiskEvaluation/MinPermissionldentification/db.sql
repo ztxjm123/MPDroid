ee76cfbfccfd83295f54ebe343c27a10ae05c90f
ztxjm123 <zt_xjm@tju.edu.cn>
2020-01-11 21:36:30 +0800

    update

--- AppRiskEvaluation/MinPermissionldentification/db.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:02c88f997a3f1be71b8ccef002600eab1281940d662de55bf0835d232619fb81
-size 975
--- AppRiskEvaluation/MinPermissionldentification/test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-echo 执行数据库备份
-echo 输出为 %1
-mysqldump -u root -p123456 analyzedata permissions permissionlog  > %1.sql
-echo 备份执行完毕
