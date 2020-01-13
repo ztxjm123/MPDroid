b1b426b2f9a1771af306ecc8fa7afbb50abf3437
ztxjm123 <zt_xjm@tju.edu.cn>
2020-01-13 08:46:48 +0800

    update

--- mpdroid/MinPermissionIdentification/db.sql
+++ mpdroid/MinPermissionIdentification/db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02c88f997a3f1be71b8ccef002600eab1281940d662de55bf0835d232619fb81
-size 975
+oid sha256:94aaffba54f2dcac8eda5237becd415cf363b0db8caf37cd6e942fd207d6c3a6
+size 995
